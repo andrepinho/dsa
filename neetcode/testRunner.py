9716211f0637e4697bed236de239d5c89eaac09f
André Pinho <andresouzapinho@gmail.com>
2025-03-21 19:19:24 -0300

    Change testRunner so it prints more concise message

--- neetcode/testRunner.py
+++ neetcode/testRunner.py
@@ -6,15 +6,17 @@ T = TypeVar('T')
 class TestRunner:
     @staticmethod
     def run_tests(solution_func: Callable[..., T], test_cases: List[Dict[str, Any]]) -> None:
-        for case in test_cases:
+        for i, case in enumerate(test_cases):
+
+            caseName = i + 1
+
             inputs = case['inputs']
             expected = case['expected']
-
             actual = solution_func(**inputs)
 
             # Display test results
             if actual == expected:
-                print(f"Case {inputs} passed.")
+                print(f"Case {caseName} passed.")
             else:
                 print(
-                    f"Case {inputs} failed. Expected {expected} but Got {actual}")
+                    f"Case {caseName} failed. Expected {expected} but Got {actual}")
